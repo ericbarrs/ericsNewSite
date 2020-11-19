818e4edab530e1f7a59f757dc7bea22a484083ef
ericbarrs <yellastar42@yahoo.com>
2020-11-19 09:33:19 -0600

    update picture

--- client1/src/info.js
+++ client1/src/info.js
@@ -1,31 +1,49 @@
-import image1 from './img/image1.png'
-import image2 from './img/image2.png'
-import image3 from './img/image3.png'
-import image4 from './img/image4.png'
+import image1 from "./img/image1.png";
+import image2 from "./img/image2.png";
+import image3 from "./img/image3.png";
+import image4 from "./img/image4.png";
+import image5 from "./img/image5.PNG";
+import image6 from "./img/image6.PNG";
 
 export const projects = [
-    {
-        image: image1,
-        description: "This app shows an example of how authentication works",
-        name: "Login Connect",
-        url: "https://loginconnect.herokuapp.com/"
-    },
-    {
-        image: image2,
-        description: "This is a project for a local business that does carpet and auto cleaning",
-        name: "Revive U Restoration",
-        url: "https://www.reviveurestoration.com/"
-    },
-    {
-        image: image3,
-        description: "This Project uses yelps api to display local restaurants",
-        name: "mylocalfoodapp",
-        url: "https://mylocalfoodapp.herokuapp.com/"
-    },
-    {
-        image: image4,
-        description: "This App was my final project for ACA i wanted to be able to save a list of exercise that would display on the users account",
-        name: "Fitness Talk",
-        url: "https://fitnesstalk.herokuapp.com/"
-    }
-]
+	{
+		image: image1,
+		description: "This app shows an example of how authentication works.",
+		name: "Login Connect",
+		url: "https://loginconnect.herokuapp.com/",
+	},
+	{
+		image: image2,
+		description:
+			"This is a project for a local business that does carpet and auto cleaning.",
+		name: "Revive U Restoration",
+		url: "https://www.reviveurestoration.com/",
+	},
+	{
+		image: image3,
+		description: "This Project uses yelps api to display local restaurants.",
+		name: "mylocalfoodapp",
+		url: "https://mylocalfoodapp.herokuapp.com/",
+	},
+	{
+		image: image4,
+		description:
+			"This App was my final project for ACA. I wanted to be able to save a list of exercise that would display on the users account.",
+		name: "Fitness Talk",
+		url: "https://fitnesstalk.herokuapp.com/",
+	},
+	{
+		image: image5,
+		description:
+			"This assignment was my final project from my Web applications class.",
+		name: "Weather app",
+		url: "https://ericsweatherapp.herokuapp.com/",
+	},
+	{
+		image: image6,
+		description:
+			"Project for week 2 User Interface design. Stlye guide for the final project.",
+		name: "Style Guide",
+		url: "https://ericbarrs.github.io/style-guide/",
+	},
+];
