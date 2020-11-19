5cc5ac244078262203a16bad8a80d54e55aa7f3a
ericbarrs <yellastar42@yahoo.com>
2020-11-19 08:24:13 -0600

    update picture

--- client1/src/components/section.js
+++ client1/src/components/section.js
@@ -2,24 +2,24 @@ import React from "react";
 import { HashLink as Link } from "react-router-hash-link";
 
 export default class Section extends React.Component {
-  render() {
-    return (
-      <div className="section" id="Home">
-        <div className="section-div">
-          <h1 className="section-h1 aw-pulsate">
-            My name is Eric and I'm a <br />
-            Full Stack Javascript Developer
-          </h1>
-          <Link smooth className="aw-arrow" to="/#aboutme" />
-        </div>
+	render() {
+		return (
+			<div className="section" id="Home">
+				<div className="section-div">
+					<h1 className="section-h1 aw-pulsate">
+						My name is Eric and I'm a <br />
+						Full Stack Javascript Developer
+					</h1>
+					<Link smooth className="aw-arrow" to="/#aboutme" />
+				</div>
 
-        <img
-          src={require("../img/img1.jpg")}
-          alt="backgrond"
-          style={{ display: "block", width: "100%" }}
-        />
-        <div className="overlay" />
-      </div>
-    );
-  }
+				<img
+					src="../img/img1.jpg"
+					alt="backgrond"
+					style={{ display: "block", width: "100%" }}
+				/>
+				<div className="overlay" />
+			</div>
+		);
+	}
 }
